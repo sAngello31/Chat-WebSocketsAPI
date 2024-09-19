0aaa9d86c9d786d99f561e35d8f62e59395bbbda
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2024-09-19 17:24:26 -0500

    Implementacion de modelos

--- server/models/messageModel.go
+++ server/models/messageModel.go
@@ -1,7 +1,9 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Message struct {
-	ID   int
+	ID   primitive.ObjectID
 	To   string
 	From string
 	Msg  string
--- server/models/userModel.go
+++ server/models/userModel.go
@@ -1,8 +1,21 @@
 package models
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type User struct {
-	ID            int
+	ID            primitive.ObjectID
 	username      string
 	password      string
 	contactNumber string
+}
+
+type UserRepository struct {
+	client *mongo.Client
+}
+
+func NewUserRepository(client *mongo.Client) *UserRepository {
+	return &UserRepository{client: client}
 }
