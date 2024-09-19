58627962ed9c8f2b232758e7a0a9b726838b2d51
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2024-09-19 17:23:50 -0500

    Arreglo de enrutamiento

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,15 +1,29 @@
 package routes
 
 import (
+	"chat_websocket/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine) {
 
-	//GET
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	//Enrutamiento de Ejemplo
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
 		})
-	})
+	}
+
+	//Auth Routes
+	auth := router.Group("/auth")
+	{
+		auth.GET("/login", controllers.ThisAuthController.Login)
+		auth.POST("/register")
+		auth.POST("/logout")
+	}
+
 }
