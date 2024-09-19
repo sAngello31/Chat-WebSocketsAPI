a6bef70f29d44cfb38b5390e761bc90fce77c4b8
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2024-09-19 17:18:29 -0500

    Configuracion de la ejecución del servidor

--- server/main.go
+++ server/main.go
@@ -1,12 +1,18 @@
 package main
 
+//1-888-420-9943
 import (
 	"chat_websocket/routes"
+	"chat_websocket/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	utils.LoadEnv()
+	utils.InitDB()
+	defer utils.CloseDB()
+	utils.InitRepositories()
 	r := gin.Default()
 	routes.SetupRoutes(r)
 	r.Run()
