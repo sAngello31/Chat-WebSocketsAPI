8bc8c113c361420d451da5883da0bb2c2df88126
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2024-09-19 17:19:15 -0500

    Implementación del Controlador de Auth

--- server/controllers/authController.go
+++ server/controllers/authController.go
@@ -1,1 +1,23 @@
 package controllers
+
+import (
+	"chat_websocket/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthController struct {
+	UserRepo *models.UserRepository
+}
+
+var ThisAuthController *AuthController
+
+func NewAuthController(userRepo *models.UserRepository) {
+	ThisAuthController = &AuthController{UserRepo: userRepo}
+}
+
+func (ctrl *AuthController) Login(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Te voy a loggear bitch",
+	})
+}
