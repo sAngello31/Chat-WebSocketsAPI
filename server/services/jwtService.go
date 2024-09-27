4da9302d0d153a9994fdd439705a77bb1a796616
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2024-09-27 18:18:32 -0500

    Validacion y recoleccion de ID por JWT

--- server/services/jwtService.go
+++ server/services/jwtService.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -19,4 +21,47 @@ func GenerateJWT(user_id primitive.ObjectID) (string, error) {
 		return "nil", err
 	}
 	return token, nil
+}
+
+func ValidJWT(token string) (jwt.Token, error) {
+	if token == "" {
+		return jwt.Token{}, fmt.Errorf("error: missing JWT")
+	}
+	tokenString := strings.TrimPrefix(token, "Bearer ")
+	realToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("STRING_TOKEN")), nil
+	})
+
+	if err != nil || !realToken.Valid {
+		return *realToken, fmt.Errorf("unexpected signing method: %v", realToken.Header["alg"])
+	}
+	return *realToken, nil
+}
+
+func GetIDFromJWT(tokenString string) (string, error) {
+	token, err := ValidJWT(tokenString)
+	if err != nil {
+		return "", err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims["id"].(string), nil
+	}
+
+	return "", fmt.Errorf("error: Token expirado o invalido")
+}
+
+func GetObjectIDFromJWT(tokenString string) (primitive.ObjectID, error) {
+	token, err := GetIDFromJWT(tokenString)
+	if err != nil {
+		return primitive.NilObjectID, err
+	}
+	objectToken, err := primitive.ObjectIDFromHex(token)
+	if err != nil {
+		return primitive.NilObjectID, err
+	}
+	return objectToken, nil
 }
