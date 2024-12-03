1caac15724da813e96ef3dfba8ff5ec850902a46
Angello Bravo <bravoangello31@gmail.com>
2024-12-03 14:22:48 -0500

    Se removió las estructuras de los controladores

--- server/controllers/chatController.go
+++ server/controllers/chatController.go
@@ -1,45 +1,23 @@
 package controllers
 
 import (
-	"chat_websocket/models"
-	"chat_websocket/services"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 )
 
-type ChatController struct {
-	MsgRepo *models.MsgRepository
-}
+func InitChat(c *gin.Context) {
+	/*
+		msgString := ("Conexion WebSocket. ID: " + token)
+		ws, err := services.Upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			log.Println("Error al conectar el websocket: ", err)
+			c.JSON(http.StatusBadRequest, "Error al conectar el websocket")
+			return
+		}
+		defer ws.Close()
+		for {
 
-var ThisChatController *ChatController
-
-func NewChatController(msgRepo *models.MsgRepository) {
-	ThisChatController = &ChatController{MsgRepo: msgRepo}
-}
-
-func (ctrl *ChatController) InitChat(c *gin.Context) {
-	tokenString := c.GetHeader("Authorization")
-	token, err := services.GetIDFromJWT(tokenString)
-	if err != nil {
-		log.Println("Acceso no Autorizado", err)
-		c.JSON(http.StatusBadRequest, "Acceso no Autorizado")
-		return
-	}
-	msgString := ("Conexion WebSocket. ID: " + token)
-	ws, err := services.Upgrader.Upgrade(c.Writer, c.Request, nil)
-	if err != nil {
-		log.Println("Error al conectar el websocket: ", err)
-		c.JSON(http.StatusBadRequest, "Error al conectar el websocket")
-		return
-	}
-	defer ws.Close()
-	for {
-
-		ws.WriteMessage(websocket.TextMessage, []byte(msgString))
-		time.Sleep(time.Second)
-	}
+			ws.WriteMessage(websocket.TextMessage, []byte(msgString))
+			time.Sleep(time.Second)
+		}
+	*/
 }
